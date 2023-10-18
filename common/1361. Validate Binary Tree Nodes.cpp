e8a326146c1722a421388b596a56c581e276d507
Sakupinera <Sakupinera@gmail.com>
2023-10-18 21:24:49 +0800

    【LeetCode】1361. Validate Binary Tree Nodes.cpp.

---
+++ common/1361. Validate Binary Tree Nodes.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+You have n binary tree nodes numbered from 0 to n - 1 where node i has two children leftChild[i] and rightChild[i], return true if and only if all the given nodes form exactly one valid binary tree.
+
+If node i has no left child then leftChild[i] will equal -1, similarly for the right child.
+
+Note that the nodes have no values and that we only use the node numbers in this problem.
+*/
+class Solution
+{
+public:
+    bool validateBinaryTreeNodes(int n, vector<int> &leftChild, vector<int> &rightChild)
+    {
+        vector<int> inDegree(n);
+        int root = -1;
+
+        // If inDegree of any node > 1, return false
+        for (const int child : leftChild)
+            if (child != -1 && ++inDegree[child] == 2)
+                return false;
+
+        for (const int child : rightChild)
+            if (child != -1 && ++inDegree[child] == 2)
+                return false;
+
+        // Find the root (node with inDegree == 0)
+        for (int i = 0; i < n; ++i)
+            if (inDegree[i] == 0)
+                if (root == -1)
+                    root = i;
+                else
+                    return false; // Multiple roots
+
+        // didn't find the root
+        if (root == -1)
+            return false;
+
+        return countNodes(root, leftChild, rightChild) == n;
+    }
+
+private:
+    int countNodes(int root, const vector<int> &leftChild, const vector<int> &rightChild)
+    {
+        if (root == -1)
+            return 0;
+        return 1 + //
+               countNodes(leftChild[root], leftChild, rightChild) +
+               countNodes(rightChild[root], leftChild, rightChild);
+    }
+};
+/*
+Example 1:
+
+Input: n = 4, leftChild = [1,-1,3,-1], rightChild = [2,-1,-1,-1]
+Output: true
+Example 2:
+
+Input: n = 4, leftChild = [1,-1,3,-1], rightChild = [2,3,-1,-1]
+Output: false
+Example 3:
+
+Input: n = 2, leftChild = [1,0], rightChild = [-1,-1]
+Output: false
+*/
+int main()
+{
+
+    return 0;
+}
